cf19faeeede228fe69faad3bd2230bf2edfef0bc
ndu shadrach <shadrachndu85@gmail.com>
2023-07-08 15:04:49 +0100

    my first push

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+function mode() {
+  let body = document.querySelector("#body");
+  let mode = body.getAttribute("data-mode");
+  if (mode == "normal") {
+    body.setAttribute("data-mode", "night");
+  } else {
+    body.setAttribute("data-mode", "normal");
+  }
+}
+
+let btn = document.querySelector("#btn");
+btn.addEventListener("click", mode);
